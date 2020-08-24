19c56c7674f179aa85b416ba9452491d09e5acd7
yo higashida <penguin4731@gmail.com>
2020-08-25 02:02:10 +0900

    ドローメニューを追加

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="openSideMenu"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>マイアドレス帳</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <SideNav />
+
+    <v-content> </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SideNav from './components/SideNav'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    SideNav,
+  },
+  data() {
+    return {
+      //
+    }
+  },
+  methods: {
+    openSideMenu() {
+      this.$store.dispatch('toggleSideMenu')
+    },
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
